a9567e970d2dde833f6730ac4fc570c9cd4bb1c6
Mykola Totsevych <totsevich02@gmail.com>
2025-12-10 23:47:23 +0100

    add <int:pk> to task complete/incomplete  to match view signature; add tag delete  pattern to enable proper tag deletion

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -9,6 +9,7 @@ from todo_app.views import (
     TagListView,
     TagCreateView,
     TagUpdateView,
+    TagDeleteView,
 )
 
 app_name = "todo_app"
@@ -18,8 +19,9 @@ urlpatterns = [
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("tasks/complete-incomplete/", complete_and_incomplete_task, name="task-complete-incomplete"),
+    path("tasks/<int:pk>/complete-incomplete/", complete_and_incomplete_task, name="task-complete-incomplete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
