ddcceb0ef0ec85932d8c96fde81766ffac8c5f04
Mykola Totsevych <totsevich02@gmail.com>
2025-12-11 00:41:47 +0100

    switch SECRET_KEY loading

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bs-rke62$ytk64bs^s1$yh#gp2l@j$je49ha-r3k5f!p%6vvl3"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
