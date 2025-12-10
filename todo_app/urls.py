9310b0cd8f4eed03d75e1c9cb40dd375625cbe67
Mykola Totsevych <totsevich02@gmail.com>
2025-12-10 22:03:52 +0100

    add task and tag CRUD routes in todo_app/urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
 
+from todo_app.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    complete_and_incomplete_task,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+)
+
 app_name = "todo_app"
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/complete-incomplete/", complete_and_incomplete_task, name="task-complete-incomplete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
 ]
